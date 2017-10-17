f6afd170231c465b6be7f336329486f13e73a496
Parthib24 <sirajammunirparthi24@gmail.com>
2017-10-17 10:33:49 +0600

    ef4x.mk: copy *.kl files

--- ef4x.mk
+++ ef4x.mk
@@ -31,7 +31,8 @@ $(call inherit-product, $(SRC_TARGET_DIR)/product/languages_full.mk)
 
 # Keylayouts
 PRODUCT_COPY_FILES += \
-	$(TREE_PATH)/keylayout/sec_touchscreen.kl:system/usr/keylayout/sec_touchscreen.kl
+	$(TREE_PATH)/keylayout/sec_touchscreen.kl:system/usr/keylayouts/sec_touchscreen.kl \
+	$(TREE_PATH)/keylayout/Generic.kl:system/usr/keylayouts/Generic.kl.kl
 
 # Media config
 MEDIA_XML_CONFIGS := \
