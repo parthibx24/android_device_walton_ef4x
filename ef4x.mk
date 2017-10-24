5e4da75c49a9ffcdbab26f5313e3b1ec693779d8
Parthib24 <sirajammunirparthi24@gmail.com>
2017-10-24 11:03:17 +0600

    Revert "ef4x.mk: Compile HexoLibre theme"
    revert commit 6ad6238fe640615ae0e63a91a94557198d5c5442.

--- ef4x.mk
+++ ef4x.mk
@@ -244,10 +244,6 @@ PRODUCT_PACKAGES += \
 # Use prebuilt kernel
 TARGET_PREBUILT_KERNEL := $(TREE_PATH)/kernel.ef4x
 
-# CM PACKAGES
-PRODUCT_PACKAGES += \
-	HexoLibre
-
 # Set those variables here to overwrite the inherited values.
 PRODUCT_NAME := full_ef4x
 PRODUCT_DEVICE := ef4x
