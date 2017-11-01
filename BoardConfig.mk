7707c58ab3639a300fd64895fbdb05679a760e66
Parthib24 <sirajammunirparthi24@gmail.com>
2017-11-01 21:22:14 +0600

    ef4x: remove kernel headers
    Moved to https://github.com/Parthib24/android_vendor_sprd

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -178,4 +178,3 @@ BOARD_SEPOLICY_DIRS += $(TREE_PATH)/sepolicy
 
 # Hack for building without kernel source
 $(shell mkdir -p $(OUT)/obj/KERNEL_OBJ/usr)
-$(shell sudo cp -a $(TREE_PATH)/headers $(OUT)/obj/KERNEL_OBJ/usr/include)
